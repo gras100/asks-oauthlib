8606ce5d7894d10b54931250b7c029550a73918d
gras100 <gras100@gmail.com>
2017-11-01 22:23:49 +0000

    async def __call__ discussion - stays pending POC.

--- asks_oauthlib/oauth1_auth.py
+++ asks_oauthlib/oauth1_auth.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 import logging
 
 from oauthlib.common import extract_params
 from oauthlib.oauth1 import Client, SIGNATURE_HMAC, SIGNATURE_TYPE_AUTH_HEADER
 from oauthlib.oauth1 import SIGNATURE_TYPE_BODY
 from asks import PostResponseAuth
-#from requests.utils import to_native_string
-#from requests.auth import AuthBase
 
 CONTENT_TYPE_FORM_URLENCODED = 'application/x-www-form-urlencoded'
 CONTENT_TYPE_MULTI_PART = 'multipart/form-data'
 
+# TODO: determine if this is safe in the curio/trio async?
 log = logging.getLogger(__name__)
 
 # OBS!: Correct signing of requests are conditional on invoking OAuth1
 # as the last step of preparing a request, or at least having the
 # content-type set properly.
-class OAuth1(AuthBase):
+# TODO: need to confirm if PostResponseAuth is always the case
+# as 3 legged authorisation for twitter user apis seems to be
+# two stages manual.
+class OAuth1(PostResponseAuth):
     """Signs the request using OAuth 1 (RFC5849)"""
 
     client_class = Client
@@ -57,10 +57,16 @@ class OAuth1(AuthBase):
             resource_owner_secret, callback_uri, signature_method,
             signature_type, rsa_key, verifier, decoding=decoding, **kwargs)
 
-    # TODO:
-    # This is inline with asks auth classes but does this need to 
-    # be an async def, as (unless oauthlib.oauth1 Client is up to 
-    # magic?) we are just prepping things here.
+    # Design decision:
+    # 1. This is inline with asks auth classes but does this need
+    # to be an async def if we are not doing any io yet? 
+    # 2. client_class can be assumed safe as asks Auth class 
+    # __init__ methods are not async, so client_class cannot 
+    # be either -- as it can't be awaited.
+    # 3. Keep async for now for consistency, but plan to 
+    # poc removal and raise as issue with asks, as outside
+    # the spirit of the trio async def/def dichotomy (what 
+    # about curio however?)
     async def __call__(self, r):
         """Add OAuth parameters to the request.
 
@@ -75,29 +81,28 @@ class OAuth1(AuthBase):
         if (not content_type and extract_params(r.body)
                 or self.client.signature_type == SIGNATURE_TYPE_BODY):
             content_type = CONTENT_TYPE_FORM_URLENCODED
-        if not isinstance(content_type, unicode):
-            content_type = content_type.decode('utf-8')
+        if not isinstance(content_type, str):
+            content_type = str(content_type,self.encoding)
 
         is_form_encoded = (CONTENT_TYPE_FORM_URLENCODED in content_type)
 
         log.debug('Including body in call to sign: %s',
                   is_form_encoded or self.force_include_body)
-
+        
+        # ASSUME this ensures r.prepare_headers call does 
+        # the right thing.
         if is_form_encoded:
             r.headers['Content-Type'] = CONTENT_TYPE_FORM_URLENCODED
+
+        if self.force_include_body:
             r.url, headers, r.body = self.client.sign(
-                unicode(r.url), unicode(r.method), r.body or '', r.headers)
-        elif self.force_include_body:
-            # To allow custom clients to work on non form encoded bodies.
-            r.url, headers, r.body = self.client.sign(
-                unicode(r.url), unicode(r.method), r.body or '', r.headers)
+                str(r.url,self.encoding), str(r.method,self.encoding), r.body or '', r.headers)
         else:
-            # Omit body data in the signing of non form-encoded requests
-            r.url, headers, _ = self.client.sign(
-                unicode(r.url), unicode(r.method), None, r.headers)
+            r.url, headers, r.body = self.client.sign(
+                str(r.url,self.encoding), str(r.method,self.encoding), None, r.headers)
 
         r.prepare_headers(headers)
-        r.url = to_native_string(r.url)
+        r.url = str(r.url,self.encoding)
         log.debug('Updated url: %s', r.url)
         log.debug('Updated headers: %s', headers)
         log.debug('Updated body: %r', r.body)
