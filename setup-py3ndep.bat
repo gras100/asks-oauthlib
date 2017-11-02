c6a840378b00d34a137e9f4622a79a841d449aab
gras100 <gras100@gmail.com>
2017-11-02 12:59:56 +0000

    install batch files added.

---
+++ setup-py3ndep.bat
@@ -0,0 +1,5 @@
+:: Idle
+set package=trio
+::"C:\Users\gras1\AppData\Local\Programs\Python\Python35-32\python.exe" -m pip install %package%
+::"C:\Users\gras1\AppData\Local\Programs\Python\Python35-32\python.exe" -m pip uninstall %package%
+"C:\Users\gras1\Anaconda3\envs\python3.5ndep\python.exe" setup.py install
