54d2e690c170c8823ef32ccabdeaa1f4f1ebee5d
gras100 <gras100@gmail.com>
2017-11-02 12:58:26 +0000

    Fixed indentation error + updated comment.

--- asks_oauthlib/__init__.py
+++ asks_oauthlib/__init__.py
@@ -1,7 +1,7 @@
 '''
 Async rework of requests-oauthlib - authors of that package take all credit!
 '''
-import logging
+import logging as _logging
 
 from .oauth1_auth import OAuth1
 from .oauth1_session import OAuth1Session
@@ -11,16 +11,18 @@ from .oauth1_session import OAuth1Session
 __version__ = '0.1.0'
 
 try:
-	import pkg_resources
-	asks_info = pkg_resources.get_distribution('asks')
-	asks_version = tuple(int(s) for s in asks_info.version.split('.'))
-	if asks_version < (1,3,6):
-		msg = ('You are using asks version %s, which is older than '
-			   'asks-oauthlib expects, please upgrade to 1.3.6 or later.')
-		raise Warning(msg % asks_version)
+    # Because asks doesn't contain a __version__ we
+    # do
+    import pkg_resources as _pkg_resources
+    _asks_info = _pkg_resources.get_distribution('asks')
+    asks_version = tuple(int(s) for s in _asks_info.version.split('.'))
+    if asks_version < (1,3,6):
+        _msg = ('You are using asks version %s, which is older than '
+               'asks-oauthlib expects, please upgrade to 1.3.6 or later.')
+        raise Warning(_msg % asks_version)
 except:
-	msg = ('Unable to determine asks version, asks-oauthlib may not work with'
-		   'asks versions ealier than 1.3.6.')
-    raise Warning(msg)
+    _msg = ('Unable to determine asks version, asks-oauthlib may not work with'
+           'asks versions ealier than 1.3.6.')
+    raise Warning(_msg)
 
-logging.getLogger('asks_oauthlib').addHandler(logging.NullHandler())
+_logging.getLogger('asks_oauthlib').addHandler(_logging.NullHandler())
